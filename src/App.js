0642ad1be8d68b5c8a4638bd850709573b0b7273
BB <bb@BBs-iMac.local>
2018-12-23 18:47:51 -0500

    new app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Yellow there!!!!!!!!!
+            Magnificento!
           </a>
         </header>
       </div>
