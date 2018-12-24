f6737586464b3257e4feeadd2c0803462b4545b0
bwitoradyo <bambang.witoradyo@gmail.com>
2018-12-23 21:32:03 -0500

    production build

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Magnificento!
+            Magnificent yeah yeah yeah...!
           </a>
         </header>
       </div>
